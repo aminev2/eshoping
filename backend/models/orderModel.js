e4b8b9772f7ec44fc04c1d9cef3e1d776c993e5a
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2023-12-11 13:50:19 +0100

    change the image and size to array

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -13,7 +13,8 @@ const orderSchema = new Schema(
       {
         name: { type: String, required: true },
         qty: { type: Number, required: true },
-        image: { type: String, required: true },
+        image: { type: Array },
+        size: { type: Array },
         price: { type: Number, required: true },
         product: {
           type: Schema.Types.ObjectId,
